63f768da4ab3ffa38638e3940fbcb6ebb0d85e82
Annie Valla <anny.valla@gmail.com>
2025-04-17 16:25:08 +0200

    Initialize first three screne

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <script setup lang="ts">
+import * as THREE from 'three'
+import { onMounted, ref } from 'vue'
+
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+
+onMounted(() => {
+  if (!canvasRef.value)
+    return
+
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  )
+  camera.position.z = 5
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasRef.value,
+    antialias: true,
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+  const animate = () => {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+  }
+  animate()
+})
 </script>
 
 <template>
-  Empty app template
+  <canvas ref="canvasRef" />
 </template>
